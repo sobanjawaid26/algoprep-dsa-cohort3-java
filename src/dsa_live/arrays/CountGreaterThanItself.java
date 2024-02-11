84ae69ebd0896ae97b2444ed7e7373ad04b0cf88
thawbaan <thawbaan@thawbaans-MacBook-Air.local>
2024-02-11 22:13:02 +0530

    CountGreaterThanItself Done, Strings Char started

--- src/dsa_live/arrays/CountGreaterThanItself.java
+++ src/dsa_live/arrays/CountGreaterThanItself.java
@@ -1,4 +1,20 @@
 package dsa_live.arrays;
 
 public class CountGreaterThanItself {
+
+    static int countGreater(int arr[], int N) {
+        int max = arr[0];
+
+        for (int i = 0; i < N; i++){
+            if(arr[i] > max)
+                max = arr[i];
+        }
+
+        int maxCount = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] ==  max)
+                maxCount++;
+        }
+        return N - maxCount;
+    }
 }
---
+++ src/dsa_live/strings/Characters1.java
@@ -0,0 +1,24 @@
+package dsa_live.strings;
+
+public class Characters1 {
+
+    public static void main(String[] args) {
+        char ch1 = 'B';
+        System.out.println((int) ch1);
+        System.out.println(ch1);
+        System.out.println("--------------------------------------");
+
+        int x = 'A';
+        x = x + 2;
+        System.out.println(x);
+        System.out.println("--------------------------------------");
+
+        //char ch3 = 'xyz'; // ERROR
+
+        System.out.println("--------------------------------------");
+        char ch4 = 66;
+        System.out.println(ch4);
+        char ch5 = (char) 66;
+        System.out.println(ch5); // SAME RESULT
+    }
+}
