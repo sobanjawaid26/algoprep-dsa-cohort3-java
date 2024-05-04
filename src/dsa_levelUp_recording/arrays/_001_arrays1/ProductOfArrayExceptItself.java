9b28b534dd43d1a134f041bfc4386e47c8c00622
thawbaan <sobanjawaid26@gmail.com>
2024-05-04 12:20:14 +0530

    test commit 1

--- src/dsa_levelUp_recording/arrays/_001_arrays1/ProductOfArrayExceptItself.java
+++ src/dsa_levelUp_recording/arrays/_001_arrays1/ProductOfArrayExceptItself.java
@@ -1,4 +1,8 @@
 package dsa_levelUp_recording.arrays._001_arrays1;
 
 public class ProductOfArrayExceptItself {
+
+    public static void main(String[] args) {
+        System.out.println("ProductOfArrayExceptItself");
+    }
 }
