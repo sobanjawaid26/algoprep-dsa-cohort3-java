01b3dd9a106f3fafeac47b65e37406ebdab64578
thawbaan <sobanjawaid26@gmail.com>
2024-06-01 13:36:22 +0530

    Leetcode Top 150 started, 88 Merge Sorted Array done

---
+++ src/lc_top_interview_150/_88_MergeSortedArray.java
@@ -0,0 +1,32 @@
+package lc_top_interview_150;
+
+public class _88_MergeSortedArray {
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(j >= 0){
+            if(i >= 0 && nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                k--;
+                i--;
+            }else{
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums1[] = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int m = 3;
+        int n = 3;
+        merge(nums1, m, nums2, n);
+        for (int i = 0; i < nums1.length; i++){
+            System.out.print(nums1[i] + " ");
+        }
+    }
+}
