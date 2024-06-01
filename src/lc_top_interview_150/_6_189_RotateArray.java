ade72f4e9473a145da00378c9b7d3d003e28a36a
thawbaan <sobanjawaid26@gmail.com>
2024-06-01 20:19:10 +0530

    189 Rotate Array added

---
+++ src/lc_top_interview_150/_6_189_RotateArray.java
@@ -0,0 +1,28 @@
+package lc_top_interview_150;
+
+public class _6_189_RotateArray {
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums , 0 , nums.length-1);
+        reverse(nums , 0 , k-1);
+        reverse(nums , k , nums.length-1);
+    }
+    public static void reverse(int[] nums , int start , int end){
+        int temp;
+        while(start<end){
+            temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {5,6,7,1,2,3,4};
+        rotate(nums, 3);
+        for (int i = 0; i < nums.length; i++){
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
