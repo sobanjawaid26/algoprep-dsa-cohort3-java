5844924612efe55e2c2de8da786a29283d571f36
thawbaan <sobanjawaid26@gmail.com>
2024-06-03 08:27:42 +0530

    135. Candy done

---
+++ src/lc_top_interview_150/_15_135_Candy.java
@@ -0,0 +1,37 @@
+package lc_top_interview_150;
+
+import java.util.Arrays;
+
+public class _15_135_Candy {
+
+    public static int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
+        }
+
+        return totalCandies;
+    }
+
+    public static void main(String[] args) {
+        int ratings[] = {1,0,2};
+        int maxCandies = candy(ratings);
+        System.out.println(maxCandies);
+    }
+}
