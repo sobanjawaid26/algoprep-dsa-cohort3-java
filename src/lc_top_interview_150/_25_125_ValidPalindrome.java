6d360a46392a0e0b5989561ac9578d7721458aab
thawbaan <sobanjawaid26@gmail.com>
2024-06-04 22:36:04 +0530

    125. Valid Palindrome added

---
+++ src/lc_top_interview_150/_25_125_ValidPalindrome.java
@@ -0,0 +1,30 @@
+package lc_top_interview_150;
+
+public class _25_125_ValidPalindrome {
+
+    // 125. Valid Palindrome
+
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+            if (!Character.isLetterOrDigit(currFirst )) {
+                start++;
+            } else if(!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+        return true;
+    }
+}
