61af9461b9588531ceba449849f9ee63ed899c0e
thawbaan <sobanjawaid26@gmail.com>
2024-06-05 06:47:36 +0530

    15. 3Sum added

---
+++ src/lc_top_interview_150/_29_15_3_Sum.java
@@ -0,0 +1,40 @@
+package lc_top_interview_150;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _29_15_3_Sum {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+                if (total > 0)
+                    k--;
+                else if (total < 0)
+                    j++;
+                else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    while (nums[j] == nums[j-1] && j < k)
+                        j++;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        List<List<Integer>> res = threeSum(nums);
+        System.out.println(res);
+    }
+}
