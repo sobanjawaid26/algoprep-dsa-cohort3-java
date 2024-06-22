6226828ee8c28eee39ca8e4f32797d352278c73d
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-06-22 22:51:02 +0530

    452 Minimum Number Of Arrows To Burst Balloons added

---
+++ src/lc_top_interview_150/_51_452_MinimumNumberOfArrowsToBurstBalloons.java
@@ -0,0 +1,34 @@
+package lc_top_interview_150;
+
+import java.util.Arrays;
+
+public class _51_452_MinimumNumberOfArrowsToBurstBalloons {
+
+    public static int findMinArrowShots(int[][] points) {
+
+        int n = points.length;
+
+        // Sort the points based on their end points
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int[] prev = points[0];
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            int currStartPoint = points[i][0];
+            int currEndPoint = points[i][1];
+
+            int prevStartPoint = prev[0];
+            int prevEndPoint = prev[1];
+
+            if (currStartPoint > prevEndPoint) {
+                count++;
+                prev = points[i];
+            } else {
+                prev[0] = Math.max(prevStartPoint, currStartPoint);
+                prev[1] = Math.min(currEndPoint, prevEndPoint);
+            }
+        }
+        return count;
+    }
+}
